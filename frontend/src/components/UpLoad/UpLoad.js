f686721c1d79521a9f46023102240bb4490a0e45
ObadaAmarneh <obada.amarneh20@gmail.com>
2022-08-05 21:04:12 +0300

    up

--- frontend/src/components/UpLoad/UpLoad.js
+++ frontend/src/components/UpLoad/UpLoad.js
@@ -5,47 +5,66 @@ import { Col, Row } from "react-bootstrap";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import React, { useState, useEffect } from "react";
+import jwt_decode from "jwt-decode";
+import Typical from "react-typical";
 // inmport fs-react from 'fs-react';
 
 import "./UpLoad.css";
 function UpLoad() {
+  const [user, setUser] = useState({});
   const navigate = useNavigate();
-
   const checkToken = async () => {
     if (!localStorage.getItem("accessToken")) {
       navigate("/Login");
+    } else {
+      setUser(jwt_decode(localStorage.getItem("accessToken")));
+      console.log("user" , user , jwt_decode(localStorage.getItem("accessToken")));
     }
-    // if (token) {
-    // }
   };
   useEffect(() => {
     checkToken();
   }, []);
   return (
-    <div className="Container">
-      <Upload.Dragger
-        multiple={true}
-        action={"http://localhost:5000/api/file/upload"}
-        listType="picture"
-        // previewFile
-        // onPreview
-        status="done"
-        beforeUpload={(file) => {
-          console.log(file);
-        }}
-        onChange={(e) => {}}
-      >
-        <div className="Upload-Container">
+    <>
+      <div className="upload-page-container">
+        <div className="typical-container">
           <Row>
-            <p className="Upload-Text">Drag files here OR</p>
-          </Row>
-          <br />
-          <Row>
-            <Button>Click to Upload</Button>
+            <Typical
+              className="text-typical"
+              steps={["Hello", 1000, "Hello world!", 500]}
+              loop={Infinity}
+              wrapper="p"
+            />
           </Row>
         </div>
-      </Upload.Dragger>
-    </div>
+        <div className="Container">
+          <Row>
+            <Upload.Dragger
+              multiple={true}
+              action={"http://localhost:5000/api/file/upload"}
+              listType="picture"
+              // previewFile
+              // onPreview
+              status="done"
+              beforeUpload={(file) => {
+                console.log(file);
+              }}
+              onChange={(e) => {}}
+            >
+              <div className="Upload-Container">
+                <Row>
+                  <p className="Upload-Text">Drag files here OR</p>
+                </Row>
+                <br />
+                <Row>
+                  <Button>Click to Upload</Button>
+                </Row>
+              </div>
+            </Upload.Dragger>
+          </Row>
+        </div>
+      </div>
+    </>
   );
 }
 export default UpLoad;
